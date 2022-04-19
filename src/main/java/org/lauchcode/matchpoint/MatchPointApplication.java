8f23b8a40dfd0b98915ec3139d220ce04350aa33
Chance Goodwin <cgood1413@gmail.com>
2022-04-19 17:47:14 -0500

    Liftoff Hello-World Assignment Completion

--- src/main/java/org/lauchcode/matchpoint/MatchPointApplication.java
+++ src/main/java/org/lauchcode/matchpoint/MatchPointApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MatchpointApplication {
+public class MatchPointApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MatchpointApplication.class, args);
+		SpringApplication.run(MatchPointApplication.class, args);
 	}
 
 }
---
+++ src/main/java/org/lauchcode/matchpoint/controllers/HelloController.java
@@ -0,0 +1,15 @@
+package org.lauchcode.matchpoint.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping(value="")
+    @ResponseBody
+    public String index(){
+        return "Hello World";
+    }
+}
