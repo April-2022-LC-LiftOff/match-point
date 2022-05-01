53fd657bb5ae6c404e3935fa25165d4e31a33a66
Anthony Rawlings <rawlings808@hotmail.com>
2022-04-30 20:11:58 -0500

    created back-end directory and front-end directory and moved files to back-end

--- back-end/src/main/java/org/lauchcode/matchpoint/controllers/HelloController.java
+++ back-end/src/main/java/org/lauchcode/matchpoint/controllers/HelloController.java
@@ -1,8 +1,11 @@
 package org.lauchcode.matchpoint.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @Controller
 public class HelloController {
