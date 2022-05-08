851e2ad4ec84019586b6fa6a64c7ebce018cc52e
hjames975 <79180645+hjames975@users.noreply.github.com>
2022-05-07 19:43:24 -0500

    Event 1

---
+++ src/main/java/org/lauchcode/matchpoint/controllers/EventController.java
@@ -0,0 +1,9 @@
+package org.lauchcode.matchpoint.controllers;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class EventController {
+
+    LocalDate ld = LocalDate.now();
+}
---
+++ src/main/java/org/lauchcode/matchpoint/models/Event.java
@@ -0,0 +1,79 @@
+package org.lauchcode.matchpoint.models;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class Event {
+
+    String firstName;
+    String lastName;
+
+    @NotNull
+    @Size(min = 1, max = 20, message = "Event name must be between 1 and 20 characters long")
+    public String eventName;
+
+    @NotNull
+    @Size(min = 1, max = 100, message = "Location must be between 1 and 100 characters long")
+    public String location;
+
+    @NotNull
+    @DateTimeFormat
+    public java.time.LocalDateTime eventDate;
+
+    @NotNull
+    @DateTimeFormat
+    public java.time.LocalDateTime eventTime;
+
+    public Event(String firstName, String lastName, String eventName, String location, LocalDateTime eventTime) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.eventName = eventName;
+        this.location = location;
+        this.eventTime = eventTime;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public LocalDateTime getEventTime() {
+        return eventTime;
+    }
+
+    public void setEventTime(LocalDateTime eventTime) {
+        this.eventTime = eventTime;
+    }
+}
