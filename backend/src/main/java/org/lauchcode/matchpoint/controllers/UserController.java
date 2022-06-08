cdcda844f917120861c2c1554241cc7d8ee63e9c
Chance Goodwin <cgood1413@gmail.com>
2022-06-08 17:43:57 -0500

    User delete API endpoint completed

--- backend/src/main/java/org/lauchcode/matchpoint/controllers/UserController.java
+++ backend/src/main/java/org/lauchcode/matchpoint/controllers/UserController.java
@@ -1,14 +1,22 @@
 package org.lauchcode.matchpoint.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.lauchcode.matchpoint.models.User;
+import org.lauchcode.matchpoint.models.data.UserRepository;
+import org.lauchcode.matchpoint.models.dto.RegisterRequestDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
 @RequestMapping("/api/user")
 public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping("/public")
     public String publicTest(){
@@ -18,5 +26,11 @@ public class UserController {
     @GetMapping("/private")
     public String privateTest(){
         return "private content";
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<?> deleteUser(@PathVariable int userId){
+        userRepository.deleteById(userId);
+        return new ResponseEntity<>("User successfully deleted", HttpStatus.OK);
     }
 }
